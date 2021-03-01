ea5961232ed32905064a93c7303ac88e2f41c002
Butanska <romi_savate@yahoo.com>
2021-03-02 00:01:51 +0200

    Implement Immutable.js

--- src/public/client.js
+++ src/public/client.js
@@ -1,14 +1,21 @@
-let store = {
-    user: { name: "Student" },
+let store = Immutable.Map({
+    user: Immutable.Map({ name: "Student" }),
     apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+    rovers: Immutable.List(['Curiosity', 'Opportunity', 'Spirit']),
+})
+
+// let store = {
+//     user: { name: "Student" },
+//     apod: '',
+//     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+// }
 
 // add our markup to the page
 const root = document.getElementById('root')
 
 const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
+    store = store.merge(store, newState)
+    //store = Object.assign(store, newState)
     render(root, store)
 }
 
@@ -20,14 +27,19 @@ const render = async (root, state) => {
 // create content
 
 const App = (state) => {
-    let { rovers, apod } = state
-  
-    if (store.currentRoverData) {
+    // let { rovers, apod } = state
+    //const rovers = state.get('rovers');
+    const apod = state.getIn(['apod','image']);
+    
+
+    //if (store.currentRoverData) {
+    if (store.get('currentRoverData')) {
       return `
         <header></header>
         <main>
             ${navBar()}
-            ${renderSelectedRoverData(store.currentRover)}         
+            <!-- {renderSelectedRoverData(store.currentRover)}  -->
+            ${renderSelectedRoverData(store.get('currentRover'))}         
   
         </main>
         <footer></footer>
@@ -38,7 +50,8 @@ const App = (state) => {
         <main>
             ${navBar()}
             ${showcase()}
-            ${Greeting(store.user.name)}          
+            <!-- {Greeting(store.user.name)} -->
+            ${Greeting(store.get('user').get('name'))}          
   
             <section>
                 <h3>Put things on the page!</h3>
@@ -51,6 +64,7 @@ const App = (state) => {
                     explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
                     but generally help with discoverability of relevant imagery.
                 </p>
+                <!-- {ImageOfTheDay(apod)} -->
                 ${ImageOfTheDay(apod)}
             </section>
         </main>
@@ -102,8 +116,9 @@ const showcase = () => {
 
 const buildNavMenu = () => {
 
-    const navArray = store.rovers;
-    
+    //const navArray = store.rovers;
+    const navArray = store.get('rovers');
+
     return navArray.map (element => {
     
         return `<div class="rovers" >
@@ -119,11 +134,17 @@ const buildNavMenu = () => {
 //Render data about selected rover
 
 const renderSelectedRoverData = (rover) => {
-    const roverName = store.currentRover
-    const launchDate = store.currentRoverData.roverData.launch_date
-    const landingDate = store.currentRoverData.roverData.landing_date
-    const status = store.currentRoverData.roverData.rover_status
-    const photoDate = store.currentRoverData.roverData.latest_photo_date
+    // const roverName = store.currentRover
+    // const launchDate = store.currentRoverData.roverData.launch_date
+    // const landingDate = store.currentRoverData.roverData.landing_date
+    // const status = store.currentRoverData.roverData.rover_status
+    // const photoDate = store.currentRoverData.roverData.latest_photo_date
+
+    const roverName = store.get('currentRover')
+    const launchDate = store.getIn(['currentRoverData', 'roverData', 'launch_date'])
+    const landingDate = store.getIn(['currentRoverData', 'roverData', 'landing_date'])
+    const status = store.getIn(['currentRoverData', 'roverData', 'rover_status'])
+    const photoDate = store.getIn(['currentRoverData', 'roverData', 'latest_photo_date'])
 
     return `<div class="roverData" >
     <h1>${roverName}</h1>
@@ -141,7 +162,9 @@ const renderSelectedRoverData = (rover) => {
 // Most recently available photos
 
 const recentPhotos = () => {
-    const photoArray = store.currentRoverData.roverData.recent_photos.photos
+    // const photoArray = store.currentRoverData.roverData.recent_photos.photos
+
+    const photoArray = store.getIn(['currentRoverData', 'roverData', 'recent_photos', 'photos'])
 
     if (photoArray.length < 4) {
         console.log(photoArray)
@@ -235,7 +258,8 @@ const ImageOfTheDay = (apod) => {
 
 // Example API call
 const getImageOfTheDay = (state) => {
-    let { apod } = state
+    //let { apod } = state
+    let apod = state.getIn(['apod','image']);
     //destructuring, equivalent to let apod = state.apod
     //console.log(state)
 
